35144a27b45ee2e772d94dfb16375f1a1d7133d0
sniperline047 <ayushsingh.be17@gmail.com>
2023-01-28 01:33:51 +0530

    learn: structres

--- src/lectures/demo/structs/structs.go
+++ src/lectures/demo/structs/structs.go
@@ -2,6 +2,43 @@ package main
 
 import "fmt"
 
-func main() {
+type Passenger struct {
+	Name         string
+	TicketNumber int
+	Boarded      bool
+}
 
+type MeleniumFalcon struct {
+	Pilot   Passenger
+	CoPilot Passenger
+}
+
+func main() {
+	leia := Passenger{"Leia", 1, false}
+	fmt.Println(leia)
+
+	var (
+		han   = Passenger{"Han", 2, false}
+		chewy = Passenger{Name: "Chewbacca", TicketNumber: 3}
+	)
+
+	fmt.Println(han, chewy)
+
+	var luke Passenger
+	luke.Name = "Luke Skywalker"
+	luke.Boarded = true
+	luke.TicketNumber = 4
+
+	fmt.Println(luke)
+
+	falcon := MeleniumFalcon{han, chewy}
+
+	han.Boarded = true
+	chewy.Boarded = true
+
+	if han.Boarded && chewy.Boarded {
+		fmt.Println(falcon.Pilot.Name, "is flying the Melinium falcon and", falcon.CoPilot.Name, "is the CoPilot")
+	} else {
+		fmt.Println("Mission failed, Empire strikes back!")
+	}
 }
--- src/lectures/exercise/structs/structs.go
+++ src/lectures/exercise/structs/structs.go
@@ -17,8 +17,67 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
+
+type Coordinate struct {
+	x float64
+	y float64
+}
+
+type Rectangle struct {
+	C1 Coordinate // top left
+	C2 Coordinate // top right
+	C3 Coordinate // bottom right
+	C4 Coordinate // bottom left
+}
+
+func isRectangle(rectangle Rectangle) bool {
+	return true
+}
+
+func dimensions(rectangle Rectangle) (float64, float64) {
+	// returns width, height
+	width := math.Sqrt(math.Pow((rectangle.C1.x-rectangle.C2.x), 2) + math.Pow((rectangle.C1.y-rectangle.C2.y), 2))
+	height := math.Sqrt(math.Pow((rectangle.C2.x-rectangle.C3.x), 2) + math.Pow((rectangle.C2.y-rectangle.C3.y), 2))
+
+	return width, height
+}
+
+func area(width float64, height float64) float64 {
+	return width * height
+}
+
+func perimeter(width float64, height float64) float64 {
+	return (width + height) * 2
+}
+
+func areaAndPerimeter(rectangle Rectangle) (float64, float64) {
+	width, height := dimensions(rectangle)
+	fmt.Println(width, height)
+	return area(width, height), perimeter(width, height)
+}
 
 func main() {
+	var (
+		coordinate1 = Coordinate{0, 0}
+		coordinate2 = Coordinate{6, 0}
+		coordinate3 = Coordinate{6, -6}
+		coordinate4 = Coordinate{0, -6}
+	)
 
+	rectangle := Rectangle{C1: coordinate1, C2: coordinate2, C3: coordinate3, C4: coordinate4}
+	fmt.Println("Input Coordinates of rectangle:", rectangle)
+
+	fmt.Println("Processing rectangle coordinates...")
+
+	if !isRectangle(rectangle) {
+		fmt.Println("[ERROR!] Input coordinates produce a invalid rectangle")
+	}
+
+	fmt.Println("Input coordinates are valid, calculating area and perimeter")
+	area, perimeter := areaAndPerimeter(rectangle)
+	fmt.Println("Area of given rectangle is", area, "and its perimeter is", perimeter)
 }
