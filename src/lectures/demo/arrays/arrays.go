e5d26b381e26f1c727764e8b3d16cd57c3a33b9a
sniperline047 <ayushsingh.be17@gmail.com>
2023-01-28 02:42:02 +0530

    learn: arrays basics

--- src/lectures/demo/arrays/arrays.go
+++ src/lectures/demo/arrays/arrays.go
@@ -2,6 +2,42 @@ package main
 
 import "fmt"
 
-func main() {
+type Room struct {
+	name    string
+	cleaned bool
+}
 
+func checkedCleanedRooms(rooms [4]Room) {
+	for i := 0; i < len(rooms); i++ {
+		room := rooms[i]
+		if room.cleaned {
+			fmt.Println(room.name, "is cleaned")
+		} else {
+			fmt.Println(room.name, "is dirty")
+		}
+	}
+}
+
+func main() {
+	// var rooms [4]Room
+
+	// rooms[0] = Room{"1", true}
+	// rooms[1] = Room{"2", true}
+	// rooms[2] = Room{"3", true}
+	// rooms[3] = Room{name: "4"}
+
+	rooms := [...]Room{
+		{name: "Room 1"},
+		{name: "Room 2"},
+		{name: "Room 3", cleaned: true},
+		{name: "Room 4", cleaned: true},
+	}
+
+	fmt.Println("Performing cleaning checks...")
+	checkedCleanedRooms(rooms)
+
+	rooms[3].cleaned = false
+
+	fmt.Println("Re-Performing cleaning checks...")
+	checkedCleanedRooms(rooms)
 }
--- src/lectures/exercise/arrays/arrays.go
+++ src/lectures/exercise/arrays/arrays.go
@@ -18,6 +18,45 @@ package main
 
 import "fmt"
 
-func main() {
+type Product struct {
+	name  string
+	price int
+}
 
+//* Print to the terminal:
+//  - The last item on the list
+//  - The total number of items
+//  - The total cost of the items
+func printStats(list [4]Product) {
+	fmt.Println("Printing product list stats")
+
+	var cost, totalItems int
+
+	for i := 0; i < len(list); i++ {
+		item := list[i]
+
+		if item.price > 0 && item.name != "" {
+			cost += item.price
+			totalItems++
+		}
+	}
+
+	fmt.Println(list[totalItems-1].name, "is the last item")
+	fmt.Println("Total items:", totalItems)
+	fmt.Println("Total cost:", cost)
+}
+
+func main() {
+	products := [4]Product{
+		{name: "Shampoo", price: 69},
+		{name: "Body Wash", price: 420},
+		{name: "Deodrant", price: 911},
+	}
+
+	fmt.Println("Current product list", products)
+	printStats(products)
+
+	fmt.Println("Adding product to list")
+	products[3] = Product{name: "Pen", price: 112}
+	printStats(products)
 }
