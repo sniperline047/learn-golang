d51323240e3519f207a257f18d612c07bf80d384
sniperline047 <ayushsingh.be17@gmail.com>
2023-02-06 00:12:08 +0530

    learn: rcv functions

--- src/lectures/demo/rcv-func/rcv-func.go
+++ src/lectures/demo/rcv-func/rcv-func.go
@@ -2,6 +2,53 @@ package main
 
 import "fmt"
 
-func main() {
+type Space struct {
+	occupied bool
+}
 
+type ParkingLot struct {
+	spaces []Space
+}
+
+func occupySpace(lot *ParkingLot, spaceNumber int) {
+	if spaceNumber > len(lot.spaces) || lot.spaces[spaceNumber-1].occupied {
+		fmt.Println("Cannot park here, Occupied!")
+		return
+	}
+
+	lot.spaces[spaceNumber-1].occupied = true
+}
+
+func (lot *ParkingLot) occupySpace(spaceNumber int) {
+	if spaceNumber > len(lot.spaces) || lot.spaces[spaceNumber-1].occupied {
+		fmt.Println("Cannot park here, Occupied!")
+		return
+	}
+
+	lot.spaces[spaceNumber-1].occupied = true
+}
+
+func (lot *ParkingLot) vacateSpace(spaceNumber int) {
+	if spaceNumber > len(lot.spaces) || !lot.spaces[spaceNumber-1].occupied {
+		fmt.Println("Space is already vacated or does not exist!")
+		return
+	}
+
+	lot.spaces[spaceNumber-1].occupied = false
+}
+
+func main() {
+	parkingLot := ParkingLot{make([]Space, 4)}
+	fmt.Println("Initial:", parkingLot)
+
+	occupySpace(&parkingLot, 2)
+	fmt.Println("Updated:", parkingLot)
+
+	parkingLot.occupySpace(3)
+	fmt.Println("Updated:", parkingLot)
+
+	parkingLot.vacateSpace(2)
+	fmt.Println("Updated:", parkingLot)
+
+	parkingLot.occupySpace(5)
 }
--- src/lectures/exercise/rcv-func/rcv-func.go
+++ src/lectures/exercise/rcv-func/rcv-func.go
@@ -16,6 +16,60 @@ package main
 
 import "fmt"
 
-func main() {
+type Stat int
+type PlayerName string
 
+type Player struct {
+	name   PlayerName
+	health Stat
+	energy Stat
+}
+
+func (player Player) defaultValues() Player {
+	if player.health == 0 {
+		player.health = 100
+	}
+
+	if player.energy == 0 {
+		player.energy = 100
+	}
+
+	return player
+}
+
+func (player *Player) changeHealth(change int) {
+	newHealth := player.health + Stat(change)
+
+	if newHealth < 0 {
+		newHealth = 0
+	}
+
+	player.health = newHealth
+}
+
+func (player *Player) changeEnergy(change int) {
+	newEnergy := player.energy + Stat(change)
+
+	if newEnergy < 0 {
+		newEnergy = 0
+	}
+
+	player.energy = newEnergy
+}
+
+func main() {
+	readyPlayerOne := Player{name: "Ready Player One"}.defaultValues()
+	fmt.Println("Initial:", readyPlayerOne)
+
+	readyPlayerOne.changeHealth(10)
+	fmt.Println("Increased Health:", readyPlayerOne)
+
+	readyPlayerOne.changeHealth(-69)
+	fmt.Println("Decreased Health:", readyPlayerOne)
+
+	readyPlayerOne.changeEnergy(69)
+	fmt.Println("Increased Energy:", readyPlayerOne)
+
+	readyPlayerOne.changeEnergy(-169)
+	fmt.Println("Decreased Energy:", readyPlayerOne)
 }
