75154aa50ec613a51c61f0039e3d90a79a335cec
sniperline047 <ayushsingh.be17@gmail.com>
2023-02-06 00:37:01 +0530

    learn: iota and variadics

--- src/lectures/demo/variadics/variadics.go
+++ src/lectures/demo/variadics/variadics.go
@@ -2,6 +2,24 @@ package main
 
 import "fmt"
 
-func main() {
+func sum(nums ...int) int {
+	sum := 0
+	for _, n := range nums {
+		sum += n
+	}
 
+	return sum
+}
+
+func main() {
+	a := []int{1, 2, 3}
+	b := []int{4, 5, 6}
+
+	all := append(a, b...)
+
+	result := sum(all...)
+	fmt.Println(result)
+
+	result = sum(1, 2, 3, 4, 5, 6)
+	fmt.Println(result)
 }
--- src/lectures/exercise/iota/iota.go
+++ src/lectures/exercise/iota/iota.go
@@ -14,7 +14,37 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+type Operation byte
+
+const (
+	add Operation = iota
+	sub
+	mul
+	div
+)
+
+func (op Operation) calculate(a, b int) int {
+	switch op {
+	case add:
+		return a + b
+	case sub:
+		return a - b
+	case mul:
+		return a * b
+	case div:
+		return a / b
+	default:
+		panic("Invalid Operation")
+	}
+}
+
+func (op Operation) calculate2(a, b int) int {
+	return []int{(a + b), (a - b), (a * b), (a / b)}[op]
+}
 
 func main() {
 	fmt.Println(add.calculate(2, 2)) // = 4
@@ -24,4 +54,12 @@ func main() {
 	fmt.Println(mul.calculate(3, 3)) // = 9
 
 	fmt.Println(div.calculate(100, 2)) // = 50
+
+	fmt.Println(add.calculate2(2, 2)) // = 4
+
+	fmt.Println(sub.calculate2(10, 3)) // = 7
+
+	fmt.Println(mul.calculate2(3, 3)) // = 9
+
+	fmt.Println(div.calculate2(100, 2)) // = 50
 }
