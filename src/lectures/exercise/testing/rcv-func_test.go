09ff890876c58d0f9335f7add31be5af367d3246
sniperline047 <ayushsingh.be17@gmail.com>
2023-02-06 02:43:26 +0530

    fix: rcv exercise and testing exercise

--- src/lectures/exercise/testing/rcv-func_test.go
+++ src/lectures/exercise/testing/rcv-func_test.go
@@ -10,3 +10,35 @@
 //
 //--Notes:
 //* Use `go test -v ./exercise/testing` to run these specific tests
+
+package main
+
+import "testing"
+
+func TestChangeHealth(t *testing.T) {
+	player := NewPlayer("ReadyPlayer1")
+
+	player.changeHealth(int(player.maxHealth) + 1)
+	if player.health > player.maxHealth {
+		t.Errorf("Health of %v cannot be more than %v, Current value: %v\n", player.name, player.maxHealth, player.health)
+	}
+
+	player.changeHealth(-(int(player.maxHealth) + 1))
+	if player.health < 0 {
+		t.Errorf("Health of %v cannot be less than 0, Current value: %v\n", player.name, player.health)
+	}
+}
+
+func TestChangeEnergy(t *testing.T) {
+	player := NewPlayer("ReadyPlayer1")
+
+	player.changeEnergy(int(player.maxHealth) + 1)
+	if player.energy > player.maxEnergy {
+		t.Errorf("Energy of %v cannot be more than %v, Current value: %v\n", player.name, player.maxEnergy, player.energy)
+	}
+
+	player.changeEnergy(-(int(player.maxHealth) + 1))
+	if player.energy < 0 {
+		t.Errorf("Energy of %v cannot be less than 0, Current value: %v\n", player.name, player.energy)
+	}
+}
